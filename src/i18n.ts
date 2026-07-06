212d49d10a762c3aa37cc493fbcbece0b5fcc9bb
plylrnsdy <plylrnsdy@163.com>
2026-07-07 01:53:43 +0800

    refactor(i18n): remove useless file

--- src/i18n.ts
+++
@@ -1,8 +0,0 @@
-import { I18n } from '@typora-community-plugin/core'
-import * as en from './locales/lang.en.json'
-
-export function createI18n() {
-  return new I18n<typeof en>({
-    localePath: './locales'
-  })
-}
